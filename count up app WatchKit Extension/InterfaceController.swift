2f945dbc220bf597f42307bf9b0342c1b686e8c5
kameyama <kameyama@kameyama-no-MacBook-Pro.local>
2016-12-18 17:18:35 +0900

    initial commit

--- count up app WatchKit Extension/InterfaceController.swift
+++ count up app WatchKit Extension/InterfaceController.swift
@@ -12,15 +12,42 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var myLabel: WKInterfaceLabel!
+    
+    var count=0
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
     
+    @IBAction func tapBtn() {
+
+        // AppGroup IDで保存したcountデータを取得する
+        let defaults = UserDefaults(suiteName: "group.app.hkameyam.test.count")
+        var count = defaults?.integer(forKey: "contValue") ?? 0
+        //カウントアップして表示する
+        count += 1
+        myLabel.setText("\(count)")
+        //カウントアップした値を保存する
+        defaults?.set(count, forKey: "contValue")
+        defaults?.synchronize()
+        
+        
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        // AppGroup IDで保存したcountデータを取得する
+        let defaults = UserDefaults(suiteName: "group.app.hkameyam.test.count")
+        let count = defaults?.integer(forKey: "contValue") ?? 0
+        // 値を表示する
+        myLabel.setText("\(count)")
+        
     }
     
     override func didDeactivate() {
--- count up app/ViewController.swift
+++ count up app/ViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func tapBtn(_ sender: Any) {
+        // AppGroup IDで保存したcountデータを取得する
+        let defaults = UserDefaults(suiteName: "group.app.hkameyam.test.count")
+        var count = defaults?.integer(forKey: "contValue") ?? 0
+        // カウントアップして表示する
+        count += 1
+        myLabel.text = "\(count)"
+        //カウントアップした値を保存する
+        defaults?.set(count, forKey: "contValue")
+        defaults?.synchronize()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // AppGroup IDで保存したcountデータを取得する
+        let defaults = UserDefaults(suiteName: "group.app.hkameyam.test.count")
+        let count = defaults?.integer(forKey: "contValue") ?? 0
+        // 値を表示する
+        myLabel.text = "\(count)"
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
